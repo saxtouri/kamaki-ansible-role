c0ab38b8e8dd5791ce1478b12b44cfb13aa589aa
Stavros Sachtouris <saxtouri@grnet.gr>
2018-11-20 12:45:21 +0200

    Create class hierarchy for Synnefo
    All Synnefo modules can import SNFCloud from
    "ansible.module_utils.synnefo".

--- library/server.py
+++ library/server.py
@@ -1,8 +1,7 @@
-# #!/usr/bin/python
+#!/usr/bin/python
 # Copyright 2018 Stavros Sachtouris <saxtouri@grnet.gr>
-import re
+from ansible.module_utils.synnefo import SNFCloud
 from kamaki.clients import ClientError
-from kamaki.clients.astakos import AstakosClient
 from kamaki.clients.cyclades import CycladesClient, CycladesNetworkClient
 from kamaki.clients.network import NetworkClient
 from kamaki.cli import logging
@@ -10,56 +9,16 @@ from kamaki.clients.utils import https
 from ansible.module_utils.basic import AnsibleModule
 
 
-class SNFCloud(AnsibleModule):
-    """Clarin-aware Synnefo methods, based on kamaki"""
-    _astakos, _cyclades, _network = None, None, None
+class SNFServer(SNFCloud):
+    """Synnefo server class, based on kamaki
+       Create, delete, start, stop, reboot, etc.
+    """
+    _cyclades, _network = None, None
 
     def __init__(self, *args, **kw):
         super(SNFCloud, self).__init__(*args, **kw)
-        self.token = self.params['cloud_token']
-        self.project_id = self.params['project_id']
-        self._handle_ssl()
-        self._check_project_id()
 
     # General purpose SNF methods and properties
-    def _handle_ssl(self):
-        ca_certs = self.params['ca_certs']
-        if ca_certs:
-            try:
-                https.patch_with_certs(ca_certs)
-            except Exception as e:
-                self.fail_json(
-                    msg="Certificates (ca_certs) failed to patch kamaki",
-                    msg_details="{}".format(e))
-        else:
-            https.patch_ignore_ssl()
-
-    def _check_project_id(self):
-        """returns: True if project id is there and active, False, otherwise"""
-        try:
-            project = self.astakos.get_project(self.project_id)
-        except ClientError as e:
-            msg='Error while checking project id "{}"'.format(self.project_id)
-            self.fail_json(msg=msg, msg_details=e.message)
-        if project['state'] != 'active':
-            msg = 'Project {name} with id {id} is not active (state: {state})'
-            self.fail_json(msg=msg.format(
-                pname=project['name'], pid=project['id'],
-                state=project['state']))
-        return True
-
-    @property
-    def astakos(self):
-        if not self._astakos:
-            try:
-                self._astakos = AstakosClient(
-                    self.params['cloud_url'], self.token)
-            except ClientError as e:
-                self.fail_json(
-                    msg="Astakos Client initialization failed",
-                    msg_details=e.message)
-        return self._astakos
-
     @property
     def cyclades(self):
         if not self._cyclades:
@@ -186,7 +145,7 @@ class SNFCloud(AnsibleModule):
 
 
 if __name__ == '__main__':
-    module = SNFCloud(
+    module = SNFServer(
         argument_spec={
             'state': {
                 'default': 'present',
@@ -203,4 +162,4 @@ if __name__ == '__main__':
             'public_ip': {'required': False, 'type': 'bool'},
         }
     )
-    module.exit_json(changed=True, msg="Cyclades operation failed")
+    module.exit_json(changed=False, msg="Cyclades operation failed".format())
